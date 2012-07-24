7d2c47255a83f90938a2228ea01a0ba59e369459
Keiichiro Ui <keiichiro.ui@gmail.com>
2012-07-24 15:59:36 +0900

    untrack screen.css

--- source/javascripts/github.js
+++ source/javascripts/github.js
@@ -4,32 +4,39 @@ var github = (function(){
     var i = 0, fragment = '', t = $(target)[0];
 
     for(i = 0; i < repos.length; i++) {
-      fragment += '<li><a href="'+repos[i].url+'">'+repos[i].name+'</a> : '+repos[i].description+'</li>';
+      fragment += '<li><a href="'+repos[i].html_url+'">'+repos[i].name+'</a> : '+
+        repos[i].description+'</li>';
     }
     t.innerHTML = fragment;
   }
   return {
     showRepos: function(options){
       $.ajax({
-          url: "http://github.com/api/v2/json/repos/show/"+options.user+"?callback=?"
+        url: "https://api.github.com/users/"+options.user+"/repos?"+
+          "type="+options.type+
+          "&sort="+options.sort+
+          "&direction="+options.direction+
+          "&callback=?"
         , type: 'jsonp'
-        , error: function (err) { $(options.target + ' li.loading').addClass('error').text("読み込み失敗"); }
+        , error: function (err) {
+          $(options.target + ' li.loading').addClass('error')
+            .text("読み込み失敗");
+        }
         , success: function(data) {
           var repos = [];
-          if (!data || !data.repositories) { return; }
-          for (var i = 0; i < data.repositories.length; i++) {
-            if (options.skip_forks && data.repositories[i].fork) { continue; }
-            repos.push(data.repositories[i]);
+          if (!data || !data.data) { return; }
+          for (var i = 0; i < options.count; i++) {
+            if (options.skip_forks && data.data[i].fork) { continue; }
+            repos.push(data.data[i]);
           }
-          repos.sort(function(a, b) {
-            var aDate = new Date(a.pushed_at).valueOf(),
-                bDate = new Date(b.pushed_at).valueOf();
+          // repos.sort(function(a, b) {
+          //   var aDate = new Date(a.pushed_at).valueOf(),
+          //   bDate = new Date(b.pushed_at).valueOf();
 
-            if (aDate === bDate) { return 0; }
-            return aDate > bDate ? -1 : 1;
-          });
+          //   if (aDate === bDate) { return 0; }
+          //   return aDate > bDate ? -1 : 1;
+          // });
 
-          if (options.count) { repos.splice(options.count); }
           render(options.target, repos);
         }
       });
